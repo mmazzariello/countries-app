27cfa21127a57541e8e0c1f1afac741a60d1e9b0
Mariana Mazzariello <mfmazzariello@gmail.com>
2022-01-08 15:11:07 +0100

    ADD ver pais component

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../intefaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.paisService.getPaisPorId(id)),
+        tap(console.log)
+      )
+      .subscribe((pais) => (this.pais = pais[0]));
   }
-
 }
+
+// this.activatedRoute.params
+//   .subscribe(({id}) => {
+//     console.log(id)
+
+//   this.PaisService.getPaisPorId(id)
+//   .subscribe( pais => {
+//     console.log(pais);
+//   })
+// })
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -4,27 +4,25 @@ import { Observable, of } from 'rxjs';
 import { Country } from '../intefaces/pais.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaisService {
-
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
-  constructor( private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   buscarPais(termino: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${termino}`;
     return this.http.get<Country[]>(url);
   }
 
-  buscarCapital(termino: string):Observable<Country[]>{
+  buscarCapital(termino: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${termino}`;
     return this.http.get<Country[]>(url);
   }
+
+  getPaisPorId(id: string): Observable<Country> {
+    const url = `${this.apiUrl}/alpha/${id}`;
+    return this.http.get<Country>(url);
+  }
 }
-
-
-
-
-
-
